5548f435a4079d812b8420ae38d4559787776438
zhangzw <zhangzw@boqii.com>
2020-09-14 17:42:02 +0800

    2020-09-14 17:42:02

--- 3-longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters.go
@@ -1,7 +1,36 @@
 package main
 
+import "fmt"
+
 //https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
-
 func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	var bitSet [256]bool
+	result, left, right := 0, 0, 0
+	for left < len(s) {
+		// 右侧字符对应的 bitSet 被标记 true，说明此字符在 X 位置重复，需要左侧向前移动，直到将X标记为 false
+		fmt.Println(bitSet[left],bitSet[s[right]],s[left],s[right],left,right)
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+		if result < right-left {
+			result = right - left
+		}
+		if left+result >= len(s) || right >= len(s) {
+			break
+		}
+	}
+	return result
+}
 
+
+func main() {
+	a := "pwwkew"
+	fmt.Println(lengthOfLongestSubstring(a))
 }
