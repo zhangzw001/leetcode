edea012ee7d5960abe38975e7d4beb2c4d3f2139
zhangzw <zhangzw@boqii.com>
2020-09-15 18:42:00 +0800

    2020-09-15 18:42:00

--- problemset-all/fastSort.go
+++ problemset-all/fastSort.go
@@ -1,15 +1,38 @@
 package main
 
+import "fmt"
 
-func fastSort(l []int ) {
-	start,end := 0, len(l)-1
-	p := l[start]
-	for start < end {
+func quickSort(arr []int, start, end int) {
+	if start < end {
+		i, j := start, end
+		key := arr[(start+end)/2]
+		for i <= j {
+			fmt.Println(arr, key)
+			for arr[i] < key {
+				i++
+			}
+			for arr[j] > key {
+				j--
+			}
+			if i <= j {
+				arr[i], arr[j] = arr[j], arr[i]
+				i++
+				j--
+			}
+			fmt.Println(start,j,end,i )
+		}
 
-		if p > l[end] {
-			l[end],l[start] = l[start],l[end]
-		}else {
-			end--
+		if start < j {
+			quickSort(arr, start, j)
+		}
+		if end > i {
+			quickSort(arr, i, end)
 		}
 	}
+}
+
+func main() {
+	a := []int{3,1,4,2,5,7}
+	quickSort(a,0,5)
+	fmt.Println(a)
 }
